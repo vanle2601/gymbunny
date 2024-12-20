3902c7b1af07428b4a4245861e61f5d8bdf18e9b
vanle <89347635+vanle2601@users.noreply.github.com>
2024-12-20 11:30:27 +1100

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,16 +1,16 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        'X-RapidAPI-Key': 'b710e8e0dcmsh90bbd0b6dec32acp17f635jsn09e8f31ac557'
+        'x-rapidapi-key': 'b710e8e0dcmsh90bbd0b6dec32acp17f635jsn09e8f31ac557',
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
     }
 };
 
 export const youtubeOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+      'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com',
+      'x-rapidapi-key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
     },
   };
   
@@ -20,5 +20,6 @@ export const fetchData = async (url, options) => {
     const data = await response.json();
     return data;
 }
+
 
   
